6f9ffa6b1286a8851962ef4a092df9fe692ae921
Lakshmi Rajkumar M <149289287+LakshmiRajkumarM@users.noreply.github.com>
2023-12-22 14:35:31 +0530

    Update fulladder_tb.v

--- fulladder_tb.v
+++ fulladder_tb.v
@@ -1,1 +1,15 @@
+module fulladder_tb;
+  reg a,b,cin;
+  wire sum,carry;
 
+  fulladder_HA dut(.a(a),.b(b),.cin(cin),.sum(sum),.carry(carry));
+  initial
+    begin
+      a=0;b=1;cin=0;
+     #5 a=1;b=1;cin=1;
+     #5 a=1;;b=1;cin=0;
+     #5 a=0;b=0;cin=0;
+
+    end
+endmodule
+  
